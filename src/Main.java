71eeb7aa4e0241cdb7537d904806427cb9072496
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-01-20 19:09:40 +1000

    заменил сообщение на Привет мир!

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("sferwfwsef");
         System.out.println("rfeаериморолролререкарr");
+        System.out.println("Привет мир!");
     }
 }
