099c2ea776870f85eed03c4ae134ccf2d8d7dd06
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-01-20 18:56:59 +1000

    initial commit5

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
         System.out.println("Hello world");
         System.out.println("sferwfwsef");
-        System.out.println("rfeаераерекарr");
+        System.out.println("rfeаериморолролререкарr");
     }
 }
